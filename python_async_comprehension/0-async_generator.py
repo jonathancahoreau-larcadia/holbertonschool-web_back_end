941824522dfee321a61813290bff115d4c06dd84
Jonathan <jonathan.cahoreau@gmail.com>
2026-06-18 10:34:22 +0200

    decimal syntax

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -15,5 +15,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        nombre = random.uniform(0, 10)
+        nombre = random.uniform(0.0, 10.0)
         yield nombre
