e7357bc89b4accb69d4b1cd5586d43e0aeed5641
Jonathan <jonathan.cahoreau@gmail.com>
2026-06-18 11:05:55 +0200

    list comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -16,12 +16,7 @@ async def measure_runtime() -> float:
         Float: Total elapsed time in seconds.
     """
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
 
     end = time.perf_counter()
 
