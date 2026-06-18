eae34614de487fba210e50dad74a6383a6691b09
Jonathan <jonathan.cahoreau@gmail.com>
2026-06-18 10:44:41 +0200

    change type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random floats with a one-second async delay between values.
 
     Yields:
